d91d63fdf502e6b1528489f596e424793e20aed5
AA199372 <kyanagi@jp.ibm.com>
2015-11-11 11:33:06 +0900

    Sample4追加

---
+++ project1/src/main/java/jp/co/iscj/sample/Sample4.java
@@ -0,0 +1,25 @@
+package jp.co.iscj.sample;
+
+public class Sample1 {
+
+	public boolean func1(String str){
+		if (str.equals("ISCJ")){
+			System.out.println("this is iscj");
+			return true;
+
+		}else{
+			return false;
+		}
+	}
+
+	public boolean func2(String str){
+		if ("ISCJ".equals(str)){
+			System.out.println("this is iscj");
+			return true;
+
+		}else{
+			return false;
+		}
+	}
+
+}
